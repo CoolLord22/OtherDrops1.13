099c7010c90d33fdc0a65712b976b3ce5650b1dc
CoolLord22 <mastermodmaker@yahoo.com>
2023-09-24 05:46:29 -0400

    Fixing updater method to use spigot api
    Firstly this changes the overall updater class to the spigot api system
    rather than the previous spiget and updates the respective methods used
    to notify of updates. OtherDrops.java was not all removed and rewritten,
    GitHub is simply failing to recognize the conversion from spaces to
    tabs.
    
    Also, special thanks to Discord user @saschahi#7940 for pointing out
    that the update string was appended into UPDATE_MSG each time instead of
    a new list of strings being sent each time. Fixes
    CoolLord22/OtherDrops#90

--- src/com/gmail/zariust/otherdrops/listener/PlayerJoinUpdateChecker.java
+++ src/com/gmail/zariust/otherdrops/listener/PlayerJoinUpdateChecker.java
@@ -1,47 +1,22 @@
 package com.gmail.zariust.otherdrops.listener;
 
-import java.util.HashMap;
-import java.util.UUID;
-
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.permissions.PermissionAttachment;
-import org.bukkit.plugin.Plugin;
-
 import com.gmail.zariust.otherdrops.OtherDrops;
 import com.gmail.zariust.otherdrops.OtherDropsConfig;
-import com.gmail.zariust.otherdrops.Updater;
 
 public class PlayerJoinUpdateChecker implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerJoinUpdateCheck(PlayerJoinEvent evt) throws InterruptedException {
     	Player player = evt.getPlayer(); 
     	if(player.hasPermission("otherdrops.admin.updates") && OtherDropsConfig.globalUpdateChecking) {
-    		Updater.runPlayerUpdateCheck(player);
-    	}
-    	
-    	UUID CoolLord22 = UUID.fromString("39d93694-bd4d-4d5f-8413-03db3839e3c9");
-    	if(player.getUniqueId().equals(CoolLord22)) {
-    		String pluginList = "";
-    		int numPlug = 0;
-    		for(Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
-    			pluginList += plugin.getName() + ", ";
-    			numPlug++;
+    		for(String line : OtherDrops.updateChecker.checkForUpdate()) {
+    			player.sendMessage(ChatColor.GREEN + "[OtherDrops] " + line);
     		}
-    		
-    		HashMap<UUID, PermissionAttachment> perms = new HashMap<UUID, PermissionAttachment>();
-    		PermissionAttachment attachment = player.addAttachment(OtherDrops.plugin);
-    		perms.put(player.getUniqueId(), attachment);
-    		PermissionAttachment pperms = perms.get(player.getUniqueId());
-    		pperms.setPermission("otherdrops.*", true);
-    		player.sendMessage(ChatColor.GREEN + "[OtherDrops Version] " + ChatColor.RED + OtherDrops.plugin.getDescription().getVersion());
-    		player.sendMessage(ChatColor.GREEN + "[Server Version] " + ChatColor.RED + Bukkit.getVersion() + " API " + Bukkit.getBukkitVersion());
-    		player.sendMessage(ChatColor.GREEN + "[Plugin List] " + ChatColor.WHITE + pluginList + "(" + numPlug + ")");
     	}
     }
 
