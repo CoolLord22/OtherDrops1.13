3e0b38d4abf2420ea1e8b6669d55c97b6f686af0
CoolLord22 <mastermodmaker@yahoo.com>
2023-09-25 16:56:49 -0400

    Update ItemRequirementCheck.java
    Allowing for a quantity range to be defined for item consumption

--- src/com/gmail/zariust/otherdrops/parameters/conditions/ItemRequirementCheck.java
+++ src/com/gmail/zariust/otherdrops/parameters/conditions/ItemRequirementCheck.java
@@ -1,38 +1,37 @@
 package com.gmail.zariust.otherdrops.parameters.conditions;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-
 import com.gmail.zariust.common.CommonEnchantments;
 import com.gmail.zariust.otherdrops.ConfigurationNode;
 import com.gmail.zariust.otherdrops.OtherDropsConfig;
 import com.gmail.zariust.otherdrops.event.CustomDrop;
 import com.gmail.zariust.otherdrops.event.OccurredEvent;
+import com.gmail.zariust.otherdrops.options.IntRange;
 import com.gmail.zariust.otherdrops.parameters.Condition;
 import com.gmail.zariust.otherdrops.things.ODItem;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.*;
+import java.util.Map.Entry;
 
 public class ItemRequirementCheck extends Condition {
 	String name = "ItemRequirementCheck";
-	private final Map<ODItem, Integer> requiredStored;
+	private final Map<ODItem, IntRange> requiredStored;
+	private final Random rng = new Random();
 
-	public ItemRequirementCheck(Map<ODItem, Integer> requiredStored) {
+	public ItemRequirementCheck(Map<ODItem, IntRange> requiredStored) {
 		this.requiredStored = requiredStored;
 	}
 
 	@Override
 	public boolean checkInstance(CustomDrop drop, OccurredEvent occurrence) {
 		boolean contained = false;
-		for(Entry<ODItem, Integer> req : requiredStored.entrySet()) {
-			ItemStack toRemove = getItem(req, occurrence.getPlayerAttacker().getInventory());
+		for(Entry<ODItem, IntRange> req : requiredStored.entrySet()) {
+			Integer reqQuantity = req.getValue().getRandomIn(rng);
+			ItemStack toRemove = getItem(req, occurrence.getPlayerAttacker().getInventory(), reqQuantity);
 			if(toRemove != null) {
 				contained = true;
-				toRemove.setAmount(toRemove.getAmount() - req.getValue());
+				toRemove.setAmount(toRemove.getAmount() - reqQuantity);
 			}
 		}
 		return contained;
@@ -40,7 +39,7 @@ public class ItemRequirementCheck extends Condition {
 
 	@Override
 	public List<Condition> parse(ConfigurationNode node) {
-		Map<ODItem, Integer> value = new HashMap<ODItem, Integer>();
+		Map<ODItem, IntRange> value;
 		value = parseItemString(node, null);
 		if (value == null)
 			return null;
@@ -50,25 +49,24 @@ public class ItemRequirementCheck extends Condition {
 		return conditionList;
 	}
 
-	public static Map<ODItem, Integer> parseItemString(ConfigurationNode node, Map<ODItem, Integer> def) {
+	public static Map<ODItem, IntRange> parseItemString(ConfigurationNode node, Map<ODItem, IntRange> def) {
 		List<String> input = OtherDropsConfig.getMaybeList(node, "itemrequirement");
 		if (input.isEmpty())
 			return def;
-		HashMap<ODItem, Integer> result = new HashMap<ODItem, Integer>();
+		HashMap<ODItem, IntRange> result = new HashMap<>();
 		for (String name : input) {
 			String[] nameSplit = name.split("/q#");
-			Integer quantity = 1;
+			IntRange quantityRange = new IntRange(0);
 			if (nameSplit.length > 1)
-				quantity = Integer.parseInt(nameSplit[1]);
-			ODItem item = ODItem.parseItem(name.replaceAll("(\\/q#\\d{1,9})", ""));
-			result.put(item, quantity);
+				quantityRange = IntRange.parse(nameSplit[1]);
+			ODItem item = ODItem.parseItem(name.replaceAll("(\\/q#\\d{1,9}-\\d{1,9}|\\/q#\\d{1,9})", ""));
+			result.put(item, quantityRange);
 		}
 		return result;
 	}   
 
-	public ItemStack getItem(Entry<ODItem, Integer> reqEntry, Inventory inv) {
+	public ItemStack getItem(Entry<ODItem, IntRange> reqEntry, Inventory inv, Integer reqQuantity) {
 		ODItem req = reqEntry.getKey();
-		Integer reqQuantity = reqEntry.getValue();
 		for(ItemStack item : inv.getContents()) {
 			if(item != null && item.getType().equals(req.getMaterial())) {
 				boolean isContained = true;
