64fcc33c7f6d42955b7b8123374b5e869bfe2fd4
CoolLord22 <mastermodmaker@yahoo.com>
2025-01-05 14:11:51 -0500

    Add tab completion for OD_ITEM@'s

--- src/com/gmail/zariust/otherdrops/OtherDropsTabExecutor.java
+++ src/com/gmail/zariust/otherdrops/OtherDropsTabExecutor.java
@@ -1,5 +1,6 @@
 package com.gmail.zariust.otherdrops;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -19,18 +20,29 @@ public class OtherDropsTabExecutor implements TabExecutor {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if(command.getName().equalsIgnoreCase("od")) {
+            ArrayList<String> list = new ArrayList<String>();
+            String lastArg = "";
             if(args.length == 1) {
                 return StringUtil.copyPartialMatches(args[0], Arrays.asList("saveitem", "id", "write", "reload", "show", "customspawn",
                         "settings", "disable", "enable", "drop", "triggers"), new ArrayList<>());
             } else if(args[0].equalsIgnoreCase("saveitem")) {
-                return StringUtil.copyPartialMatches(args[1], List.of("<item key>"), new ArrayList<>());
+                list.add("<item key>");
+                for(NamespacedKey key : OtherDrops.loadedItems.keySet()) {
+                    list.add(key.getKey().replaceAll("od_item_", "OD_ITEM@"));
+                }
+                lastArg = args[1];
             } else if(args[0].equalsIgnoreCase("id")) {
                 return StringUtil.copyPartialMatches(args[1], Arrays.asList("mob", "block"), new ArrayList<>());
             } else if(args[0].equalsIgnoreCase("show")) {
                 return StringUtil.copyPartialMatches(args[1], List.of("<block>"), new ArrayList<>());
             } else if(args[0].equalsIgnoreCase("drop")) {
-                return StringUtil.copyPartialMatches(args[1], List.of("<drop string>"), new ArrayList<>());
+                list.add("<drop string>");
+                for(NamespacedKey key : OtherDrops.loadedItems.keySet()) {
+                    list.add(key.getKey().replaceAll("od_item_", "OD_ITEM@"));
+                }
+                lastArg = args[1];
             }
+            return StringUtil.copyPartialMatches(lastArg, list, new ArrayList<>());
         }
         return null;
     }
